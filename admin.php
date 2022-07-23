1a2973600b9b59fbbcea5074462edc0618d9d3f9
chinh108 <57782333+chinh108@users.noreply.github.com>
2022-07-23 10:58:40 +0700

    up

---
+++ admin.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin page</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./css/admin.css">
+
+</head>
+
+<body>
+    <div class="main">
+        <h1 style="text-align: center; font-size:40px; font-weight:bold; padding-top:20px"><span>Admin Page</span></h1>
+        
+        <section class="Admin">
+            <div class="container">
+                <main class="row">
+                    <div class="thumb-item-flow col-lg-6">
+                        <div class="tumb-wrapper">
+
+                        </div>
+                    </div>
+
+                    <div class="thumb-item-flow col-lg-6">
+                        <div class="tumb-wrapper">
+                            
+                        </div>
+                    </div>
+                </main>
+
+                <main class="row">
+                    <div class="thumb-item-flow col-lg-6">
+                        <div class="tumb-wrapper">
+
+                        </div>
+                    </div>
+
+                    <div class="thumb-item-flow col-lg-6">
+                        <div class="tumb-wrapper">
+                            
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </section>
+    </div>
+</body>
